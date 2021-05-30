50dbaea3db5cc14f7f598b42267295e40758c3f8
Yadnyesh Bharat Juvekar <yadnyesh@users.noreply.github.com>
2021-05-30 21:29:02 +0530

    Simple API working

--- src/main/java/io/yadnyesh/springsecurity/student/StudentController.java
+++ src/main/java/io/yadnyesh/springsecurity/student/StudentController.java
@@ -6,13 +6,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
 
+    private static final List<Student> STUDENT_LIST = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Anna Bond"),
+            new Student(3, "Dr. No"),
+            new Student(4, "Moonraker"),
+            new Student(5, "Diamonds are forever")
+    );
+
     @GetMapping(path="{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
-
+        return STUDENT_LIST.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student: " + studentId + " does not exist."));
     }
 
 }
--- src/main/java/io/yadnyesh/springsecurity/student/model/Student.java
+++ src/main/java/io/yadnyesh/springsecurity/student/model/Student.java
@@ -1,10 +1,11 @@
 package io.yadnyesh.springsecurity.student.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Student {
     private final Integer studentId;
     private final String studentName;
-    public Student(Integer studentId, String studentName) {
-        this.studentId = studentId;
-        this.studentName = studentName;
-    }
 }
